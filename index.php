619f8a156419fab30d3dc2411d07ced51584d148
NaeLike เนไลก์ <87599521+Naelike@users.noreply.github.com>
2022-04-17 16:46:00 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+  // texteng
+  $texteng = 'Hello Naelike';
+  // echo
+  echo strtoupper($texteng).'<br>';
+  echo strtolower($texteng).'<br>';
+  echo ucfirst($texteng).'<br>';
+  echo ucwords($texteng).'<br>';
+  echo mb_strtoupper($texteng).'<br>';
+?>
